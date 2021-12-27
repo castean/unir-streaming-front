2f2fd96f74af39208cdfa89e2774ba10c9b3885f
Antonio Castellanos <ing.castean@gmail.com>
2021-12-27 12:54:00 -0700

    first commit

---
+++ components/NavBar.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="navbar">
+        <h1>UNIR - Streaming App</h1>
+    </div>
+</template>
+
+<style scoped>
+.navbar{
+    display:flex;
+    background-color: #0098cd;
+    justify-content: center;
+    align-items: center;
+}
+h1{
+    color:#ffffff;
+}
+</style>
